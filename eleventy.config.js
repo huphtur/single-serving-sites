7ec7e6852cd6dafb755180ef10baf6a8774bbba9
Marcel Appelman <huphtur@icloud.com>
2024-02-18 18:19:14 -0800

    new card design

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,7 @@
 module.exports = function (eleventyConfig) {
-  // https://github.com/huphtur/hella-simple-eleventy-tailwind-starter
-  eleventyConfig.setServerOptions({
-    watch: ['_site/**/*.css'],
-  });
+
+  eleventyConfig.addPassthroughCopy("src/bundle.css");
+  // eleventyConfig.addPassthroughCopy("src/bundle.js");
 
   // https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
@@ -11,18 +10,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/icon-192.png');
   eleventyConfig.addPassthroughCopy('src/icon-512.png');
   eleventyConfig.addPassthroughCopy('src/manifest.webmanifest');
-  
+
   eleventyConfig.addPassthroughCopy('src/meta.png');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
   eleventyConfig.addPassthroughCopy('src/humans.txt');
 
   // https://huphtur.nl/eleventy-filter-to-turn-url-into-domain-name/
-  eleventyConfig.addFilter("domainify", function(string) {
-    return new URL(string).hostname.replace('www.','');
+  eleventyConfig.addFilter("domainify", function (string) {
+    return new URL(string).hostname.replace('www.', '');
   });
 
   eleventyConfig.addFilter('values', Object.values);
-  
+
   return {
     dir: {
       input: 'src',
