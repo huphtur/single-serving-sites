78cdfb4cd5d6109615a6e7565e60b213506c8fe3
Andy Baio <andyb@xoxofest.com>
2025-04-04 10:06:50 -0700

    Add RSS feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,8 @@
 const htmlmin = require("html-minifier-terser");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
 
   eleventyConfig.addPassthroughCopy("src/bundle.css");
 
