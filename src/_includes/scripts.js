07e3c4478b82c1bb2f6b7776d51b64cb0aa25c9b
Marcel Appelman <huphtur@icloud.com>
2026-03-01 17:37:33 +0100

    replace list.js with custom script

--- src/_includes/scripts.js
+++
@@ -1,6 +0,0 @@
-const siteList = new List('sites', {
-	valueNames: ["domain", { name: "launch", attr: "datetime" }, "title", "description", "author"],
-	page: 69,
-	pagination: true,
-});
-siteList.sort("launch", { order: "desc" });
