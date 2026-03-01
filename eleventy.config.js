a2d6a91b042aa8c8d180178bf8b1f629f449e3e0
Marcel Appelman <huphtur@icloud.com>
2026-03-01 18:48:07 +0100

    show hosts on serve

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,7 @@
 const htmlmin = require("html-minifier-terser");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
 
   eleventyConfig.addPassthroughCopy("src/bundle.css");
@@ -26,9 +26,9 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter('values', Object.values);
 
-  eleventyConfig.addTransform("htmlmin", function(content) {
+  eleventyConfig.addTransform("htmlmin", function (content) {
     // Prior to Eleventy 2.0: use this.outputPath instead
-    if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
@@ -40,6 +40,10 @@ module.exports = function(eleventyConfig) {
     }
 
     return content;
+  });
+
+  eleventyConfig.setServerOptions({
+    showAllHosts: true,
   });
 
   return {
